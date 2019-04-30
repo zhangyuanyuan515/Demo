dd05f5aab82cf0976fc374e017815f10001c4f9d
zyy <1973822011@qq.com>
2019-04-30 09:36:02 +0800

    修改阮鸡鸡

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hhhhhhhhhhddddd
+        //阮鸡鸡大坏蛋
     }
 }
